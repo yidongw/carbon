2f4c52e392acc96a4f2886b7bec86ae038b27626
Sidwebworks <58144379+sidwebworks@users.noreply.github.com>
2026-05-19 18:43:28 +0530

    feat: add MES jump actions to jobs (#786)
    This reverts commit 7fa7bcafbf7c2860a8b9185dd6dfd68eea8cc747.

--- apps/erp/app/modules/production/ui/Jobs/JobBillOfProcess.tsx
+++ apps/erp/app/modules/production/ui/Jobs/JobBillOfProcess.tsx
@@ -77,6 +77,7 @@ import {
   LuMaximize2,
   LuMinimize2,
   LuPaperclip,
+  LuPlay,
   LuRefreshCcw,
   LuSend,
   LuSettings2,
@@ -276,6 +277,26 @@ function makeItem(
             dueDate={operation.dueDate ?? null}
           />
           <JobOperationTags operation={operation} availableTags={tags} />
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <a
+                href={path.to.external.mesJobOperation(operation.id!)}
+                title={t`Open in MES`}
+              >
+                <IconButton
+                  icon={<LuPlay />}
+                  variant="secondary"
+                  aria-label={t`Open in MES`}
+                  size="sm"
+                />
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <span>
+                <Trans>Open in MES</Trans>
+              </span>
+            </TooltipContent>
+          </Tooltip>
           <Tooltip>
             <TooltipTrigger asChild>
               <Link
