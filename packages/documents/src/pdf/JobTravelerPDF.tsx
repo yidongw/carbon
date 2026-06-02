e77e9cb3376ee01a561957b2b30e5791718f1f46
Gaurav Bhatt <bhattgauravd@gmail.com>
2026-06-02 17:10:02 +0530

    added a tiny label

--- packages/documents/src/pdf/JobTravelerPDF.tsx
+++ packages/documents/src/pdf/JobTravelerPDF.tsx
@@ -267,7 +267,11 @@ export const JobTravelerPageContent = ({
           company={company}
           title="Job Traveler"
           documentId={job.jobId}
-          documentSubId={job.salesOrderReadableId ?? undefined}
+          documentSubId={
+            job.salesOrderReadableId
+              ? `SO# ${job.salesOrderReadableId}`
+              : undefined
+          }
           date={job.startDate}
           locale={locale}
         />
